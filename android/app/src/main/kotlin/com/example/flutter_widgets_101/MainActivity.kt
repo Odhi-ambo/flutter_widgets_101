725d494ead63b2f31db9a717b57d0cec934d8a6d
Odhi-ambo <michaelderrick605@gmail.com>
2024-05-01 18:12:14 +0300

    app dependecies

---
+++ android/app/src/main/kotlin/com/example/flutter_widgets_101/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_widgets_101
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/homepage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          height: 200,
+          width: 200,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: Colors.deepPurple,
+          ),
+        ),
+      ),
+    );
+  }
+}
